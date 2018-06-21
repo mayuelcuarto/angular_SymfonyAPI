5ac7474c77705fd6c2f9f2fcae70ea504fc9b361
mayuelcuarto <cristhianmayuri@gmail.com>
2018-06-20 20:11:36 -0500

    Proceso para crear el registro de usuario

--- src/app/components/register.component.ts
+++ src/app/components/register.component.ts
@@ -1,21 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
 
 @Component({
 	selector: 'register',
-	templateUrl: '../views/register.html'
+	templateUrl: '../views/register.html',
+	providers: [UserService]
 })
 export class RegisterComponent implements OnInit{
 	public title: string ;
+	public user: User;
+	public status;
 
 	constructor(
-		//private _route: ActivatedRoute,
-		//private _router: Router
-		){
-		this.title = 'Componente de register';
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private _userService: UserService
+	){
+		this.title = 'Registro';
+		this.user = new User(1, "user", "", "", "", "");
 	}
 	
 	ngOnInit(){
 		console.log('El componente register.component ha sido cargado!!');
+	}
+
+	onSubmit(){
+		console.log(this.user);
+		this._userService.register(this.user).subscribe(
+			response => {
+				this.status = response.status;
+				if(response.status != 'Success'){
+					this.status = 'Error';
+				}else{
+					this.user = new User(1, "user", "", "", "", "");
+				}
+			},
+			error => {
+				console.log(<any>error)
+			}
+			);
 	}
 }
