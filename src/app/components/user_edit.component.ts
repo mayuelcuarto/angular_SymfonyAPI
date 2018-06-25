3c735a7d6c69f30209f1e77217f263c210583efa
mayuelcuarto <cristhianmayuri@gmail.com>
2018-06-25 15:58:29 -0500

    Componente de edición de usuario

---
+++ src/app/components/user_edit.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user';
+
+@Component({
+	selector: 'user-edit',
+	templateUrl: '../views/user_edit.html',
+	providers: [UserService]
+})
+export class UserEditComponent implements OnInit {
+	public title: string;
+	public user: User;
+	public status;
+	public identity;
+	public token;
+
+	constructor(
+		private _userService: UserService,
+		private _route: ActivatedRoute,
+		private _router: Router
+		){
+		this.title = 'Modificar mis datos';
+		this.identity = this._userService.getIdentity();
+		this.token = this._userService.getToken();
+	}
+
+	ngOnInit(){
+		if(this.identity == null){
+			this._router.navigate(['/login']);
+		}else{
+			this.user = new User(
+				this.identity.sub,
+				this.identity.role,
+				this.identity.name,
+				this.identity.surname,
+				this.identity.email,
+				this.identity.password
+				);
+		}
+	}
+
+	onSubmit(){
+		console.log(this.user);
+		this._userService.update_user(this.user).subscribe(
+			response => {
+				this.status = response.status;
+				if(response.status != 'Success'){
+					this.status = 'Error';
+				}else{
+					localStorage.setItem('identity', JSON.stringify(this.user));
+				}
+			},
+			error => {
+				console.log(<any>error)
+			}
+			);
+	}
+}
