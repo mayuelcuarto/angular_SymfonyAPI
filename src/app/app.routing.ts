ca33e4d85e6c7175e553863e2a44b767630732f3
mayuelcuarto <cristhianmayuri@gmail.com>
2018-06-25 15:57:36 -0500

    Añadiendo ruta de edición de usuario

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './components/default.component';
 import { LoginComponent } from './components/login.component';
 import { RegisterComponent } from './components/register.component';
+import { UserEditComponent } from './components/user_edit.component';
 
 const appRoutes: Routes = [
 	{path:'', component:DefaultComponent},
 	{path:'login', component:LoginComponent},
 	{path:'login/:id', component:LoginComponent},
 	{path:'register', component:RegisterComponent},
+	{path:'user-edit', component:UserEditComponent},
 	{path:'**', component:LoginComponent}
 ];
 
